6531f61c85c7d0f2c000f42e0a8910cd8f634a09
Christer Dale <rchristerdale@gmail.com>
2024-03-08 02:26:03 +0800

    still fixing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,81 +1,47 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, UnauthenticatedError } = require('../errors');
-const uploadProfilePicture = require('../middleware/fileUpload').handleProfilePictureUpload;
-const uploadLicensePicture = require('../middleware/fileUpload').handleLicensePictureUpload;
-
+const uploadImage = require('../middleware/fileUpload'); // Import multer middleware
 
 const register = async (req, res) => {
-  try {
-      // Extract user data from request body
-      const { role, fullName, email, ...otherUserData } = req.body;
+    try {
+        // Upload the image using the middleware
+        uploadImage(req, res, async function (err) {
+            if (err) {
+                return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Image upload failed', error: err.message });
+            }
 
-      // Check if the user is a Doctor
-      if (role === 'Doctor') {
-          // Upload the profile picture and license picture using the middleware
-          uploadProfilePicture(req, res, async function(err) {
-              if (err) {
-                  return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Profile picture upload failed', error: err.message });
-              }
 
-              uploadLicensePicture(req, res, async function(err) {
-                  if (err) {
-                      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'License picture upload failed', error: err.message });
-                  }
+            // Check if an image was uploaded and get its download URL
+            const imageURL = req.imageURL || null;
 
-                  // Check if profile picture and license picture were uploaded successfully
-                  const imageData = req.profilePictureURL || null; // imageData
-                  const imageLicense = req.licensePictureURL || null; //imageLicense
+            // Extracting other file data from the request
+            const { ...userData } = req.body;
 
-                  // Create the user with profile and license picture URLs
-                  const user = await User.create({
-                      role,
-                      fullName,
-                      email,
-                      imageData,
-                      imageLicense,
-                      ...otherUserData
-                  });
+            // Add imageURL to user data if it exists
+            const userDataWithImage = imageURL ? { ...userData, imageData: imageURL } : userData;
 
-                  // Generate JWT token
-                  const token = user.createJWT();
+            try {
+                // Create the user with image data if available
+                const user = await User.create(userDataWithImage);
 
-                  // Send response with user details and token
-                  res.status(StatusCodes.CREATED).json({ user: { role, fullName, email, profilePictureURL: imageData, licensePictureURL }, token });
-              });
-          });
-      } else {
-          // If the user is not a Doctor, skip license picture upload
-          // Upload only the profile picture
-          uploadProfilePicture(req, res, async function(err) {
-              if (err) {
-                  return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Profile picture upload failed', error: err.message });
-              }
+                // Generate JWT token
+                const token = user.createJWT();
 
-              // Check if profile picture was uploaded successfully
-              const imageData = req.profilePictureURL || null;
-
-              // Create the user with only the profile picture URL
-              const user = await User.create({
-                  role,
-                  fullName,
-                  email,
-                  imageData,
-                  ...otherUserData
-              });
-
-              // Generate JWT token
-              const token = user.createJWT();
-
-              // Send response with user details and token
-              res.status(StatusCodes.CREATED).json({ user: { role, fullName, email, profilePictureURL }, token });
-          });
-      }
-  } catch (error) {
-      // Handle database or server errors
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Registration failed', error: error.message });
-  }
-};
+                // Send response
+                res.status(StatusCodes.CREATED).json({ user: { name: user.Fname }, role: { role: user.role }, token });
+            } catch (error) {
+                // Handle validation errors
+                if (error.name === 'ValidationError') {
+                    const errors = Object.values(error.errors).map(err => err.message);
+                    return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Validation failed', errors });
+                }
+                throw error; // Re-throw other errors
+            }
+        });
+    } catch (err) {
+        // Handle database or server errors
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Registration failed', error: err.message });
 
 
 
--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,37 +1,43 @@
-const { StatusCodes } = require("http-status-codes");
-const multer = require("multer");
-const path = require("path");
-const { getStorage, ref, uploadBytes, getDownloadURL  } = require("firebase/storage");
+require('dotenv').config();
 
-// Initialize Firebase app and storage
 const { initializeApp, getApp } = require("firebase/app");
-const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-};
-const firebaseApp = initializeApp(firebaseConfig);
+const { getStorage, ref, uploadBytes, getDownloadURL } = require("firebase/storage");
+const multer = require("multer");
+const { StatusCodes } = require("http-status-codes");
+const path = require("path"); // Import the path module
+
+// Rest of your middleware code...
+// Check if Firebase app is already initialized
+let firebaseApp;
+try {
+    firebaseApp = getApp();
+} catch (error) {
+    const firebaseConfig = {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId,
+        measurementId: process.env.measurementId
+    };
+    firebaseApp = initializeApp(firebaseConfig);
+}
 const storage = getStorage(firebaseApp);
 
 // Multer configuration for handling image uploads
-const multerConfig = multer({
+const upload = multer({
     storage: multer.memoryStorage(),
     limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
-    fileFilter: function(req, file, cb) {
+    fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
     }
-});
-
+}).single("image");
 // Check file type
 function checkFileType(file, cb) {
     const filetypes = /jpeg|jpg|png|gif/;
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = filetypes.test(file.mimetype);
-
     if (mimetype && extname) {
         return cb(null, true);
     } else {
@@ -47,50 +53,25 @@ async function uploadImageToStorage(file) {
     return downloadURL;
 }
 
-// Middleware function to handle profile picture upload and save the URL to req.profilePictureURL
-const uploadProfilePicture = multerConfig.single("image");
-const handleProfilePictureUpload = (req, res, next) => {
-    uploadProfilePicture(req, res, async function(err) {
+// Middleware function to handle image uploads and save image URL to req.imageURL
+function uploadImage(req, res, next) {
+    upload(req, res, async function (err) {
         if (err instanceof multer.MulterError) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: "Profile picture upload failed: " + err.message });
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: "Multer error: " + err.message });
         } else if (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error uploading profile picture: " + err.message });
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error uploading image: " + err.message });
         }
         if (!req.file) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: "No profile picture uploaded" });
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: "No image uploaded" });
         }
-
         try {
-            const profilePictureURL = await uploadImageToStorage(req.file);
-            req.profilePictureURL = profilePictureURL; // Save the profile picture URL in the request object
+            const imageURL = await uploadImageToStorage(req.file);
+            req.imageURL = imageURL; // Save the image URL in the request object
             next(); // Call the next middleware or route handler
         } catch (error) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error uploading profile picture to Firebase Storage: " + error.message });
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error uploading image to Firebase Storage: " + error.message });
         }
     });
-};
+}
 
-// Middleware function to handle license picture upload and save the URL to req.licensePictureURL
-const uploadLicensePicture = multerConfig.single("licensePicture");
-const handleLicensePictureUpload = (req, res, next) => {
-    uploadLicensePicture(req, res, async function(err) {
-        if (err instanceof multer.MulterError) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: "License picture upload failed: " + err.message });
-        } else if (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error uploading license picture: " + err.message });
-        }
-        if (!req.file) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: "No license picture uploaded" });
-        }
-
-        try {
-            const licensePictureURL = await uploadImageToStorage(req.file);
-            req.licensePictureURL = licensePictureURL; // Save the license picture URL in the request object
-            next(); // Call the next middleware or route handler
-        } catch (error) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error uploading license picture to Firebase Storage: " + error.message });
-        }
-    });
-};
-
-module.exports = { handleProfilePictureUpload, handleLicensePictureUpload };
+module.exports = uploadImage;
